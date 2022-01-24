6f4ae1c9696020236fd203ca15399e198c7f0e59
Firespindash <firespindash@gmail.com>
2022-01-24 16:04:05 -0300

    Code cleanup and script update.

--- README.md
+++ README.md
@@ -1,9 +1,9 @@
 # custom-resolution-x11
 A program to set custom resolutions in xorg. It's a script to set probably any screen resolution you want to. (**default custom resolution is 1920x989**). \
-This program was tested in openbox and i3wm, but it may work in any interface using X11. The script was made to run in bash and X11. 
+This program was tested in openbox and i3wm, but it may work in any interface using X11. This script was mint to be used for VMs.
 
 ## Instructions
-Run the script as root user. Before using the script write the resolution you want in the variable *resolution* inside the script and make sure you have xrandr, cvt and sudo installed. When writing the resolution you want to set, put the 2 numbers separated without an x between them. To run this program type on terminal `./setresolution.sh`. When it finish work put the 'fixscreen.sh' script into the initialization of programs of your graphical interface. Then reboot your machine.
+Run the script as root user. To run this program type on terminal `./setresolution.sh`. When it finishes work put the 'fixscreen.sh' script into the initialization of programs of your graphical interface. Then reboot your machine. If your environment overrides the resolution, you will need to set the new resolution using your graphical interface.
 
 ## Usability
-This program is not mented to work with multiple monitors. And it set just one resolution a time. The '10-monitor.conf' file is to add permanently the custom resolution. With it when you reboot the machine the resolution will already be set. The 'fixscreen.sh' file is to set the resolution to screen when you run it at the initialization or when you just run it. If you have some issues you can look [here](https://wiki.archlinux.org/index.php/Xrandr). The program is based in the Arch Wiki Xrandr guide. Depending of the issue make sure your system is updated.
+This script was made to work only on single-head monitor configurations. And it sets just one resolution a time. The script creates a '10-monitor.conf' file to add permanently the custom resolution. With it, when you reboot the machine the resolution will already be set. And a 'fixscreen.sh' file for you to set the resolution to the screen on your graphical environment, if it doesn't override it. If you have some issues you can look [here](https://wiki.archlinux.org/index.php/Xrandr). This script is based in the Arch Wiki Xrandr guide. Depending on the issue make sure your system is updated.
--- setresolution.sh
+++ setresolution.sh
@@ -3,60 +3,58 @@
 # Author: Firespindash
 # License: MIT
 
-if [[ $EUID != 0 ]] 
-then
-    printf "Please run as root user.\n"
-    exit 
-fi
+xorgpath="/etc/X11/xorg.conf.d"
 
-resolution="1920 989"  # set the custom resolution here.
+[ $EUID != 0 ] && { printf "Please run as root user.\n"; exit; }
 
-printf "Before using this script make sure you have xrandr and cvt installed. \n"
-printf "And write the resolution you want in the variable 'resolution' of the script. \n\n" && sleep 5
+command -v xrandr > /dev/null
+[ $? -eq 0 ] || { printf "This script needs xrandr to run.\n"; exit; }
+command -v cvt > /dev/null
+[ $? -eq 0 ] || { echo "This script needs cvt to run."; exit; }
+
+read -p "Choose the custom resolution you want? [1920x989] " resolution
+[ "$resolution" ] && resolution="${resolution/x/ }" || resolution="1920 989"
+
+cvt $resolution > /dev/null 2> /dev/null
+[ $? -eq 0 ] || { echo "Not a valid input."; exit; }
 
 printf "Detecting monitor name...\n"
 
-monitor=$(xrandr | grep " connected " | awk '{ print$1 }')
+monitor=$(xrandr | grep " connected " | awk '{ print $1 }')
 
 printf "Detecting resolution...\n"
 
-preferredresolution=$(cvt $resolution | grep Modeline | cut -d '"' -f2 | cut -d'_' -f1)
+preferredresolution=$(cvt $resolution | grep Modeline | cut -d '"' -f2 | cut -d '_' -f1)
 
-fix_resolution() { cvt $resolution | grep Modeline | cut -f2 | cut -d'_' -f1; }
-detect_properties() { cvt $resolution | grep Modeline | cut -d '"' -f3 | cut -d'_' -f1; }
+fix_resolution() { cvt $resolution | grep Modeline | cut -f2 | cut -d '_' -f1; }
+detect_properties() { cvt $resolution | grep Modeline | cut -d '"' -f3 | cut -d '_' -f1; }
 
-newfixedresolution="$(echo "$(cvt $resolution | grep Modeline | cut -d ' ' -f2 | cut -d'_' -f1)"'"'"$(detect_properties)")"
-
+newfixedresolution="$(echo "$(cvt $resolution | grep Modeline | cut -d ' ' -f2 | cut -d '_' -f1)"'"'"$(detect_properties)")"
 finalresolution="$(echo "$(fix_resolution)"'"'"$(detect_properties)")"
 
-printf "Writing the resolution...\n"
+printf "Trying to write the new resolution...\n"
 
-xrandr --newmode $newfixedresolution
+xrandr --newmode $newfixedresolution 
+xrandr --addmode $monitor $preferredresolution 2> /dev/null
 
-xrandr --addmode $monitor $preferredresolution
+[ -f $xorgpath/10-monitor.conf ] &&
+    cp $xorgpath/10-monitor.conf $xorgpath/10-monitor.conf.bak
 
-if [[ -f 10-monitor.conf && ! -f 10-monitor.conf.bak ]]
-then
-    cp /etc/X11/xorg.conf.d/10-monitor.conf /etc/X11/xorg.conf.d/10-monitor.conf.bak
-fi
+echo 'Section "Monitor"' > $xorgpath/10-monitor.conf
+echo "	Identifier \"$monitor\"" >> $xorgpath/10-monitor.conf
+echo "	$finalresolution" >> $xorgpath/10-monitor.conf
+echo "	Option \"PreferredMode\" \"$preferredresolution\"" >> $xorgpath/10-monitor.conf
+echo 'EndSection' >> $xorgpath/10-monitor.conf
 
-echo 'Section "Monitor"' > /etc/X11/xorg.conf.d/10-monitor.conf
-echo "	Identifier \"$monitor\"" >> /etc/X11/xorg.conf.d/10-monitor.conf
-echo "	$finalresolution" >> /etc/X11/xorg.conf.d/10-monitor.conf
-echo "	Option \"PreferredMode\" \"$preferredresolution\"" >> /etc/X11/xorg.conf.d/10-monitor.conf
-echo 'EndSection' >> /etc/X11/xorg.conf.d/10-monitor.conf
+printf "Trying to set the new resolution...\n"
 
-printf "Setting the resolution...\n"
-
-xrandr --output $monitor --mode $preferredresolution
-
-command=$(echo "xrandr --output $monitor --mode $preferredresolution")
+xrandr --output $monitor --mode $preferredresolution 2> /dev/null
 
 printf "Generating command on file...\n"
 
 sudo echo '#!/bin/bash' > ./fixscreen.sh
-sudo echo ' ' >> ./fixscreen.sh
-sudo echo "$command" >> ./fixscreen.sh
+sudo echo '' >> ./fixscreen.sh
+sudo echo "xrandr --output $monitor --mode $preferredresolution" >> ./fixscreen.sh
 
 printf "Making file executable...\n\n"
 
